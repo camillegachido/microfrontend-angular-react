5c67193cca9ca6d3bed96526957ce7185e27ef22
Camille Gachido <camillegachido70@gmail.com>
2023-04-06 13:51:38 -0300

    add(main-app): join aplications into container

---
+++ main-app/src/CamilleBuus-root-config.js
@@ -0,0 +1,21 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+    name: "@CamilleBuus/sub-app",
+    app: () => System.import("@CamilleBuus/sub-app"),
+    activeWhen: ["/"],
+})
+
+registerApplication({
+  name: '@CamilleBuus/sub-app-angular',
+  app: () => System.import('@CamilleBuus/sub-app-angular'),
+  activeWhen: ["/"]
+});
+
+start({
+  urlRerouteOnly: true,
+});
+
+start({
+  urlRerouteOnly: true,
+});
